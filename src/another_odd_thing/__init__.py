1a3bd744c6a64ffdbbdc1146e5530f5b57da93d6
anttiluode <67085641+anttiluode@users.noreply.github.com>
2026-09-16 06:06:36 +0300

    feat: add resident operator package

---
+++ src/another_odd_thing/__init__.py
@@ -0,0 +1,11 @@
+"""AnotherOddThing: resident operators probed through small addressed events."""
+
+from .core import ResidentOperator, event_bit, passive_trace, probe_once, v0_operators
+
+__all__ = [
+    "ResidentOperator",
+    "event_bit",
+    "passive_trace",
+    "probe_once",
+    "v0_operators",
+]
